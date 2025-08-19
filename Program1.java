3b196905d45595b0b9bce180b4bf329ed819c84a
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-08-19 18:42:12 +0530

    learn for upstream

--- Program1.java
+++ Program1.java
@@ -4,6 +4,9 @@ public class Program1 {
     public static void main(String[] args) {
         System.out.println("hamza is my second name");
         System.out.println("added new line here");
+
+        System.out.println("adding for upstream git push -u origin main");
+        System.out.println("final......!");
     }
 
 }
