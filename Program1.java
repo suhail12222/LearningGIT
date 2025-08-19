543e9872c5bd403cafec12a628a2e1a0542288dc
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-08-19 18:38:36 +0530

    new line added

--- Program1.java
+++ Program1.java
@@ -3,6 +3,7 @@ package one;
 public class Program1 {
     public static void main(String[] args) {
         System.out.println("hamza is my second name");
+        System.out.println("added new line here");
     }
 
 }
